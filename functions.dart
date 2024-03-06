ba95901d11a732fecacaded3610cf64cd779a566
Sylvostika <sylvesterbamidele@gmail.com>
2024-03-06 11:55:55 +0100

    my week 2 dart assignment

---
+++ functions.dart
@@ -0,0 +1,27 @@
+// a fuction that adds two numbers
+int addTwo(int a, int b){
+    return a + b;
+}
+// a function that subtracts two numbers
+int subtractTwo(int a, int b){
+    return a - b;
+}
+
+// a function that multiplies two numbers
+int multiplyTwo(int a, int b){
+    return a * b;
+}
+//a function that divides two numbers
+
+int divideTwo(int a, int b){
+    return a / b;
+}
+//a function that takes an argument of type string
+int stringLength(String input){
+    return input.length;
+}
+// a function that takes a list as an argument
+var myList = [2, 4, 6, 8, 10]
+dynamic getFirstElement(myList){
+        return myList[0]
+    }
